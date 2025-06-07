0461769913956b6afd552b2fa9865906b844b06d
okada <s.okada@vision-sys.co.jp>
2025-06-07 15:51:21 +0900

    トップページ作成

---
+++ src/main/java/com/example/demo/controller/TopController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@EnableAutoConfiguration
+public class TopController {
+
+	// トップ画面
+	@GetMapping({ "/"})
+	public String top() {
+		// templates/index.htmlにリンクされる
+		return "index";
+	}
+}
