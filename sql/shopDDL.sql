481ac95dadcc568d67a82f508b01cd1523ea5a5e
DESKTOP-OP0BTK7\渡辺 瑛太 <e.watanabe@vision-sys.co.jp>
2025-06-18 14:31:35 +0900

    no message

--- sql/shopDDL.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE products (
-  `barcode` long(20) NOT NULL COMMENT '管理番号（バーコード）',
-  `name` string(100) NOT NULL COMMENT '商品名',
-  `cost_price` int(10,2) NOT NULL COMMENT '仕入れ値',
-  `sale_price` int(10,2) NOT NULL COMMENT '売値',
-  UNIQUE KEY `products_IX1` (`barcode`)
--- sql/webAppDDL.sql
+++ sql/webAppDDL.sql
@@ -4,3 +4,10 @@ CREATE TABLE users (
     email VARCHAR(100) UNIQUE,          -- メールアドレス (一意)
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP  -- 作成日時
 );
+
+CREATE TABLE products (
+  `barcode` long(20) NOT NULL COMMENT '管理番号（バーコード）',
+  `name` string(100) NOT NULL COMMENT '商品名',
+  `cost_price` int(10,2) NOT NULL COMMENT '仕入れ値',
+  `sale_price` int(10,2) NOT NULL COMMENT '売値',
+  UNIQUE KEY `products_IX1` (`barcode`)
