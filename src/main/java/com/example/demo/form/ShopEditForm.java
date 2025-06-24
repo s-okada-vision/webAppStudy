19938374449e43087e328d5923e52f9046f6088d
DESKTOP-OP0BTK7\渡辺 瑛太 <e.watanabe@vision-sys.co.jp>
2025-06-24 10:59:50 +0900

    no message

---
+++ src/main/java/com/example/demo/form/ShopEditForm.java
@@ -0,0 +1,43 @@
+package com.example.demo.form;
+
+import java.math.BigDecimal;
+//情報をデータベースに登録するメソッド
+public class ShopEditForm {
+
+	private String barcode;
+	private String name;
+	private BigDecimal costPrice;
+	private BigDecimal salePrice;
+	
+	public String getBarcode() {
+		return barcode;
+	}
+
+	public void setBarcode(String barcode) {
+		this.barcode = barcode;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public BigDecimal getCostPrice() {
+		return costPrice;
+	}
+	
+	public void setCostPrice(BigDecimal cost_price) {
+		this.costPrice = cost_price;
+	}
+	
+	public BigDecimal getSalePrice() {
+		return salePrice;
+	}
+	
+	public void setSalePrice(BigDecimal sale_price) {
+		this.salePrice = sale_price;
+	}
+}
